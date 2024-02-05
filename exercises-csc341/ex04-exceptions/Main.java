63c7b162717aebbaef1292e3a34465a9402dea1e
Amy C. Larson <larsonam@augsburg.edu>
2024-02-05 13:07:17 -0600

    adding status print statements

--- exercises-csc341/ex04-exceptions/Main.java
+++ exercises-csc341/ex04-exceptions/Main.java
@@ -5,9 +5,14 @@ public class Main {
 		MyClass mc = new MyClass();
 		
 		// make this work so that everything exits "gracefully"
-		
+
+		System.out.println("\nCalling funC from main");
 		mc.funC();
+
+		System.out.println("\nCalling funB from main");
 		mc.funB();
+
+		System.out.println("\nCalling funA from main");
 		mc.funA();
 	
 	}
